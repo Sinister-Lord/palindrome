094d3022f86b39408fa6f95d45cee972abac9768
Sinister-Lord <80533597+Sinister-Lord@users.noreply.github.com>
2025-02-20 23:34:15 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+number = int(input("Enter your number: "))
+
+temp = number
+reversed_number = 0
+
+while temp > 0:
+  digit = temp % 10
+  reversed_number = reversed_number * 10 + digit
+  temp //= 10
+
+if number == reversed_number:
+  print(f"{number} is a palindrome")
+else:
+  print(f"{number} is not a palindrome")
